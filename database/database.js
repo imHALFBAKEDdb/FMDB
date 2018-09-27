f99b0c3ea7e98c4ad75d023aec9dcf73786f4146
Julie Im <julieim@Julies-MacBook-Air.local>
2018-09-26 17:04:27 -0700

    changed everything to point to localhost

--- database/database.js
+++ database/database.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-var mongoUri = "mongodb://mongo:27017/test";
+var mongoUri = "mongodb://localhost:27017/test";
 // var addDataToDatabase = require("./helperFunctions.js").addDataToDatabase;
 var movieData = require("./movieData.js");
 
@@ -45,44 +45,44 @@ var movieSchema = new mongoose.Schema(
 
 var Movies = mongoose.model("Movies", movieSchema);
 
-var addDataToDatabase = function(input, callback) {
-  for (var i = 0; i < input.length; i++) {
-    //console.log(input[i]["Title"]);
-    var instance = new Movies({
-      Title: input[i]["Title"],
-      Year: input[i]["Year"],
-      imdbID: input[i]["imdbID"],
-      Type: input[i]["Type"],
-      Poster: input[i]["Poster"],
-      review_average: input[i]["review_average"],
-      review_count: input[i]["review_count"],
-      extra_images: input[i]["extra_images"],
-      director: input[i]["director"],
-      writer: input[i]["writer"],
-      release_date: input[i]["release_date"],
-      movie_length: input[i]["movie_length"],
-      movie_rating: input[i]["movie_rating"],
-      actor_info: input[i]["actor_info"],
-      movie_genre: input[i]["movie_genre"],
-      review_date: input[i]["review_date"],
-      reviewer: input[i]["reviewer"],
-      review_title: input[i]["review_title"],
-      reviews_number: input[i]["reviews_number"],
-      review_viewers: input[i]["review_viewers"],
-      review: input[i]["review"],
-      movie_description: input[i]["movie_description"],
-      movie_id: input[i]["id"]
-    })
-      .save()
-      .then(() => {
-        console.log("saved to db");
-      })
-      .catch(err => {
-        console.log("there was an error", err);
-      });
-    // instance.update({ _id: input[i]["id"] });
-  }
-};
+// var addDataToDatabase = function(input, callback) {
+//   for (var i = 0; i < input.length; i++) {
+//     //console.log(input[i]["Title"]);
+//     var instance = new Movies({
+//       Title: input[i]["Title"],
+//       Year: input[i]["Year"],
+//       imdbID: input[i]["imdbID"],
+//       Type: input[i]["Type"],
+//       Poster: input[i]["Poster"],
+//       review_average: input[i]["review_average"],
+//       review_count: input[i]["review_count"],
+//       extra_images: input[i]["extra_images"],
+//       director: input[i]["director"],
+//       writer: input[i]["writer"],
+//       release_date: input[i]["release_date"],
+//       movie_length: input[i]["movie_length"],
+//       movie_rating: input[i]["movie_rating"],
+//       actor_info: input[i]["actor_info"],
+//       movie_genre: input[i]["movie_genre"],
+//       review_date: input[i]["review_date"],
+//       reviewer: input[i]["reviewer"],
+//       review_title: input[i]["review_title"],
+//       reviews_number: input[i]["reviews_number"],
+//       review_viewers: input[i]["review_viewers"],
+//       review: input[i]["review"],
+//       movie_description: input[i]["movie_description"],
+//       movie_id: input[i]["id"]
+//     })
+//       .save()
+//       .then(() => {
+//         console.log("saved to db");
+//       })
+//       .catch(err => {
+//         console.log("there was an error", err);
+//       });
+//     // instance.update({ _id: input[i]["id"] });
+//   }
+// };
 
 db.once("open", () => {
   var dbAdded = false;
--- database/helperFunctions.js
+++ database/helperFunctions.js
@@ -1,50 +1,51 @@
-// var Movies = require("./database.js");
-// var myData = require("./movieData.js");
-// var mongoose = require("mongoose");
+var Movies = require("./database.js");
+var myData = require("./movieData.js");
+var mongoose = require("mongoose");
 
-// // var myData = JSON.stringify(myData);
-// //initialize data in databse - only run once
+// var myData = JSON.stringify(myData);
+//initialize data in databse - only run once
 
-// var addDataToDatabase = function(input) {
-//   for (var i = 0; i < input.length; i++) {
-//     //console.log(input[i]["Title"]);
-//     new Movies({
-//       Title: input[i].Title,
-//       Year: input[i]["Year"],
-//       imdbID: input[i]["imdbID"],
-//       Type: input[i]["Type"],
-//       Poster: input[i]["Poster"],
-//       review_average: input[i]["review_average"],
-//       review_count: input[i]["review_count"],
-//       extra_images: input[i]["extra_images"],
-//       director: input[i]["director"],
-//       writer: input[i]["writer"],
-//       release_date: input[i]["release_date"],
-//       movie_length: input[i]["movie_length"],
-//       movie_rating: input[i]["movie_rating"],
-//       actor_info: input[i]["actor_info"],
-//       movie_genre: input[i]["movie_genre"],
-//       review_date: input[i]["review_date"],
-//       reviewer: input[i]["reviewer"],
-//       review_title: input[i]["review_title"],
-//       reviews_number: input[i]["reviews_number"],
-//       review_viewers: input[i]["review_viewers"],
-//       review: input[i]["review"],
-//       movie_description: input[i]["movie_description"]
-//     })
-//       .save()
-//       .then(() => {
-//         console.log("saved to db");
-//       })
-//       .catch(err => {
-//         console.log("there was an error", err);
-//       });
-//   }
-// };
+var addDataToDatabase = function(input) {
+  for (var i = 0; i < input.length; i++) {
+    //console.log(input[i]["Title"]);
+    new Movies({
+      Title: input[i].Title,
+      Year: input[i]["Year"],
+      imdbID: input[i]["imdbID"],
+      Type: input[i]["Type"],
+      Poster: input[i]["Poster"],
+      review_average: input[i]["review_average"],
+      review_count: input[i]["review_count"],
+      extra_images: input[i]["extra_images"],
+      director: input[i]["director"],
+      writer: input[i]["writer"],
+      release_date: input[i]["release_date"],
+      movie_length: input[i]["movie_length"],
+      movie_rating: input[i]["movie_rating"],
+      actor_info: input[i]["actor_info"],
+      movie_genre: input[i]["movie_genre"],
+      review_date: input[i]["review_date"],
+      reviewer: input[i]["reviewer"],
+      review_title: input[i]["review_title"],
+      reviews_number: input[i]["reviews_number"],
+      review_viewers: input[i]["review_viewers"],
+      review: input[i]["review"],
+      movie_description: input[i]["movie_description"],
+      movie_id: input[i]["id"]
+    })
+      .save()
+      .then(() => {
+        console.log("saved to db");
+      })
+      .catch(err => {
+        console.log("there was an error", err);
+      });
+  }
+};
 
-// // addDataToDatabase(myData);
+ addDataToDatabase(myData);
 
-// //find data from database
+//find data from database
 // var retrieved = function(cb) {
 //   Movies.find({})
 //     .then(res => {
@@ -55,4 +56,4 @@
 //     });
 // };
 
-// module.exports.addDataToDatabase = addDataToDatabase;
+module.exports.addDataToDatabase = addDataToDatabase;
