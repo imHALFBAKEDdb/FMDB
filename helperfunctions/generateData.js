e28e325e6477540e02b53ec547776d558f600273
Julie Im <julieim@Julies-MacBook-Air.local>
2018-09-27 16:38:09 -0700

    generated 10M data

--- helperfunctions/generateData.js
+++ helperfunctions/generateData.js
@@ -1,58 +1,59 @@
-var faker = require('faker');
-var fs = require('fs');
+var faker = require("faker");
+var fs = require("fs");
+const writefile = fs.createWriteStream("./helperfunctions/data.json");
 
 var randomMovieLength = () => {
-  var randomhours = Math.floor((Math.random() * 2) + 1);
-  var randomminutes = Math.floor((Math.random() * 59) + 1);
+  var randomhours = Math.floor(Math.random() * 2 + 1);
+  var randomminutes = Math.floor(Math.random() * 59 + 1);
   var hours = "hours";
   var minutes = "minutes";
-  if(randomhours === 1) {
-    hours = "hour"
+  if (randomhours === 1) {
+    hours = "hour";
   }
-  if(randomminutes === 1) {
-    minutes = "minute"
+  if (randomminutes === 1) {
+    minutes = "minute";
   }
   return `${randomhours} ${hours} ${randomminutes} ${minutes}`;
-}
+};
 
 var randomActorInfo = () => {
   let arr = [];
-  let randomNum = Math.floor((Math.random() * 5) + 1);
+  let randomNum = Math.floor(Math.random() * 5 + 1);
   var randomObj = {
-    "charactor_name": '',
-    "actor_name": ''
-  }
-  for(let i = 0; i < randomNum; i++) {
+    charactor_name: "",
+    actor_name: ""
+  };
+  for (let i = 0; i < randomNum; i++) {
     randomObj.charactor_name = faker.name.findName();
     randomObj.actor_name = faker.name.findName();
     arr.push(randomObj);
   }
   return arr;
- }
+};
 
 var generate10Mdata = () => {
   var bigData = [];
   var obj = {
-    "Title": '',
-    "Year": '',
-    "imdbID": '',
-    "Type": 'movie',
-    "Poster": '',
-    "review_average": '',
-    "review_count": '',
-    "extra_images": '',
-    "director": '',
-    "writer": '',
-    "release_date": '',
-    "movie_length": '',
-    "actor_info": ''
-  }
-  for(let i = 0; i < 1000000; i++) {
+    Title: "",
+    Year: "",
+    imdbID: "",
+    Type: "movie",
+    Poster: "",
+    review_average: "",
+    review_count: "",
+    extra_images: "",
+    director: "",
+    writer: "",
+    release_date: "",
+    movie_length: "",
+    actor_info: ""
+  };
+  for (let i = 0; i < 10000000; i++) {
     obj.Title = faker.commerce.productName();
     obj.Year = faker.date.past();
     obj.imdbID = "tt" + faker.random.number();
     obj.Poster = faker.image.people();
-    obj.review_average = Math.floor((Math.random() * 5) + 1);
+    obj.review_average = Math.floor(Math.random() * 5 + 1);
     obj.review_count = faker.random.number();
     obj.extra_images = faker.image.animals();
     obj.director = faker.name.findName();
@@ -60,13 +61,13 @@ var generate10Mdata = () => {
     obj.release_date = faker.date.past();
     obj.movie_length = randomMovieLength();
     obj.actor_info = randomActorInfo();
-    bigData.push(obj);
-    console.log(i)
+    writefile.write(JSON.stringify(obj));
+    console.log(i);
   }
-  return bigData;
-}
+};
 
-fs.writeFile("data.json", JSON.stringify(generate10Mdata(), null, 2), (err) => {
-  if(err) throw err;
-  console.log("the file has been saved!")
-})
+generate10Mdata();
+// fs.writeFile("data1.json", JSON.stringify(generate10Mdata(), null, 2), (err) => {
+//   if(err) throw err;
+//   console.log("the file has been saved!")
+// })
